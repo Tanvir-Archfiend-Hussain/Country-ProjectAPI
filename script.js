91c0d47490008c6fdbe060764272268bfc366f43
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-04-23 19:22:30 +0100

    JS

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("country-inp");
+
+/** When Search Button.... */
+searchBtn.addEventListener("click", () => {
+  let countryName = countryInp.value;
+  let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+});
